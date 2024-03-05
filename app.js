a7faa26c9eb1f2bbe6575c5edc757248f8287cb6
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-03-06 00:37:10 +0500

    added all

---
+++ app.js
@@ -0,0 +1,47 @@
+// chapter9to11
+//Q1
+// var city=prompt("Enter your city name")
+// if(city=="karachi"){
+//     document.write("the city of light")
+// }
+// else{
+//     document.write("not a light city ")
+// }
+
+// Q2
+// var male="male"
+// var female="female"
+// var gender=prompt("Enter your gender")
+// if(gender>=male){
+//     alert("Good Morning sir")
+
+// }
+// else if( gender>=female){
+//     alert("Good Morning Mamm")
+
+// }
+// else{
+//     alert("Please try again")
+// }
+
+
+// Q3
+
+
+// var red="red";
+// var yellow="yellow";
+// var green="green";
+// var signal=prompt("Road Trafic Signal")
+// if(signal==red){
+//     document.write("<table boder=1px>"+"<tr ><th>Signal</th> <th>Message</th> </tr> <tr><td>Red</td> <td>Must Stop</td></tr>"+"</table>")
+
+// }
+// else if(signal==yellow){
+//     document.write("<table boder=1px>"+"<tr ><th>Signal</th> <th>Message</th> </tr> <tr><td>Yello</td> <td>Ready to Move</td></tr>"+"</table>")
+
+// }
+// else if(signal==green){
+//     document.write("<table boder=1px>"+"<tr ><th>Signal</th> <th>Message</th> </tr> <tr><td>Green</td> <td>Move Now</td></tr>"+"</table>")
+
+// }
+
