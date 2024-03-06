723de5385e4f95b65602923b9a232d69ca060ed4
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-03-07 01:15:48 +0500

    update all

--- app.js
+++ app.js
@@ -45,3 +45,294 @@
 
 // }
 
+// Q4
+// var a="“Please refill the fuel in your car”";
+// var b=prompt("Fule in car","Search your fule limit");
+// if(b<=0.25){
+//     alert(a)
+// };
+// Q5
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// // true
+// }
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// // false
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// // false
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// // true
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// // false
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// // true
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// // true
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+// Q6
+// skip
+// Q7
+// var firstName=10;
+// var my_prompt=prompt("Gusse the Secrat Number")
+// if(my_prompt>firstName){
+
+// alert("Bingo!The currect Answer")
+// }
+//  if(my_prompt<=firstName){
+// alert("Close enough to the correct answer")
+// }
+// Q8
+// var a=prompt("Give the number divisible by 3")
+// if(a=3){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=6){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=9){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=12){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=15){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=18){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=21){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=24){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=27){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+// else if(a=30){
+//     alert(
+//         "the number is divisible by 3"
+//     )
+// }
+
+// Q9
+
+// var a=prompt("Cheak Even and Odd Number ")
+// if(a==2){
+//     alert("this is even number")
+// }
+// else if(a==4){
+//     alert("this is even number")
+// }
+// else if(a==6){
+//     alert("this is even number")
+// }
+// else if(a==8){
+//     alert("this is even number")
+// }
+// else if(a==10){
+//     alert("this is even number")
+// }
+// else if(a==12){
+//     alert("this is even number")
+// }
+// else if(a==14){
+//     alert("this is even number")
+// }
+// else if(a==16){
+//     alert("this is even number")
+// }
+// else if(a==18){
+//     alert("this is even number")
+// }
+// else if(a==20){
+//     alert("this is even number")
+// }
+
+
+// else if(a==1){
+//     alert("this is odd number")
+// }
+// else if(a==3){
+//     alert("this is odd number")
+// }
+// else if(a==5){
+//     alert("this is odd number")
+// }
+// else if(a==7){
+//     alert("this is odd number")
+// }
+// else if(a==9){
+//     alert("this is odd number")
+// }
+// else if(a==11){
+//     alert("this is odd number")
+// }
+// else if(a==13){
+//     alert("this is odd number")
+// }
+// else if(a==15){
+//     alert("this is odd number")
+// }
+// else if(a==17){
+//     alert("this is odd number")
+// }
+// else if(a==19){
+//     alert("this is odd number")
+// }
+// else if(a==21){
+//     alert("this is odd number")
+// }
+// else if(a==23){
+//     alert("this is odd number")
+// }
+// else if(a==25){
+//     alert("this is odd number")
+// }
+// else if(a==27){
+//     alert("this is odd number")
+// }else if(a==29){
+//     alert("this is odd number")
+// }
+// Q10
+// var a=prompt("Weather Temperature")
+// if(a>40){
+//     alert("It is too hot outside")
+
+// }
+// else if(a>30){
+// alert("The Weather today is Normal")
+// }
+// else if(a>20){
+//     alert("Today’s Weather is cool")
+//     }
+//     else if(a>=10){
+//         alert("OMG! Today’s weather is so Cool")
+//         }
+// Q11
+// var firstNum=2;
+// var secandNum=6;
+// var opreation=firstNum+secandNum-firstNum*secandNum/firstNum%secandNum
+// // 2+6-2*6/2%6
+// document.write(opreation)
+
+
+
+// chapter12to13
+// Q1
+// var A=65;
+// var a=97;
+// var Z=90;
+// var z=122;
+// var b =prompt()
+// if(b==="A"){
+// alert(A)
+// }
+// else if(b==="a"){
+// alert(a)
+// }
+// else if(b==="Z"){
+//     alert(Z)
+//     }
+//     else if(b==="z"){
+//         alert(z)
+//         }
+// Q2
+// var a=120
+// var b=240
+// var c=prompt("Enter greater number")
+// if(c<a){
+//     alert("this is less then 'a'")
+// }
+// else if(c==a){
+//     alert("this is equal to 'a'")
+
+// }
+// if(c>b){
+//     alert("this is greater then 'b'")
+// }
+// else if(c==b){
+//     alert("this is equal to 'b'")
+
+// }
+// else{
+//     alert(
+//         "this not a equal"
+//     )
+// }
+
+// Q3
+// SKIP
+
+// Q4
+// SKIP
+
+// Q5
+// var password=prompt("Enter your password","Enter your password")
+// if(password=="sufiyan"){
+// alert("Currect   password")
+// }
+// else{
+//     alert(
+//         "Incurrect"+"  "+ "password"
+//     )
+// }
+
+// Q6
+// var greeting;
+// var hour =prompt()
+// if (hour < 18) {
+// alert(greeting = "Good day")
+
+// }else{
+//     alert(
+//         greeting = "Good evening"
+//     )
+// }
+// Q7
+// skip
